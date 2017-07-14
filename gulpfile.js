c34d85df812406555d7df9cfeecd5382fed58c2b
Nallely House <nallelyht@gmail.com>
2017-07-14 15:04:23 -0500

    cambiando index

--- gulpfile.js
+++ gulpfile.js
@@ -1,64 +1,75 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var bowserify = require('gulp-bowserify');
+var browserify = require('gulp-browserify');
 var rename = require('gulp-rename');
+
 var browserSync = require('browser-sync').create();
+
 var config = {
-	source: './src',
-	dist: './public'
+	source: './src/',
+	dist: './public/'
 };
+
 var paths = {
 	assets: "assets/",
 	html: "**/*.html",
-	js: 'js/**/*.js',
 	sass: "scss/**/*.scss",
 	mainSass: "scss/main.scss",
 	mainJS : "js/app.js"
 };
+
 var sources = {
-	assets:config.source + paths.assets,
-	html:config.source + paths.html,
+	assets: config.source + paths.assets,
+	html: config.source + paths.html,
 	sass: paths.assets + paths.sass,
 	js: config.source + paths.js,
 	rootSass: config.source + paths.assets + paths.mainSass,
 	rootJS: config.source + paths.assets + paths.mainJS
 };
+
+
 gulp.task('html', function() {
-	gulp.src(sources.html)
-		.pipe(gulp.dest(config.dist));
+	gulp.src(sources.html).pipe(gulp.dest(config.dist));
 });
-gulp.task('sass', function() {
+
+gulp.task("sass", function() {
 	gulp.src(sources.rootSass)
 		.pipe(sass({
-		outputStyle: 'compressed'
-	}).on('error', sass.logError))
-		.pipe(gulp.dest(config.dist + paths.assets + 'css'));
+		outputStyle: "compressed"
+	}).on("error", sass.logError))
+		.pipe(gulp.dest( config.dist + paths.assets+ "css"));
 });
-gulp.task('js', function(){
+
+gulp.task("js", function() {
 	gulp.src(sources.rootJS)
 		.pipe(browserify())
-		.pipe(rename('bundle.js'))
-		.pipe(gulp.dest(config.dist + paths.assets + 'js'));
+		.pipe(rename("bundle.js"))
+		.pipe(gulp.dest(config.dist + paths.assets + "js"));
 });
-gulp.task("sass-watch", ['sass'], function (done){
+
+gulp.task("sass-watch", ["sass"], function (done) {
 	browserSync.reload();
 	done();
 });
-gulp.task('js-watch', ['js'], function (done){
+
+gulp.task("js-watch", ["js"], function (done) {
 	browserSync.reload();
 	done();
 });
-gulp.task('html-watch', ['html'], function (done){
+
+gulp.task("html-watch", ["html"], function (done) {
 	browserSync.reload();
 	done();
 });
-gulp.task('serve', function(){
+
+gulp.task("serve", function() {
 	browserSync.init({
 		server: {
-			baseDir: config.dist
+			baseDir: config.build
 		}
 	});
-	gulp.watch(sources.html, ['html-watch']);
-	gulp.watch(sources.sass, ['sass-watch']);
-	gulp.watch(sources.js, ['js-watch']);
+
+	gulp.watch(sources.html, ["html-watch"]);
+	gulp.watch(sources.sass, ["sass-watch"]);
+	gulp.watch(sources.js, ["js-watch"]);
 });
